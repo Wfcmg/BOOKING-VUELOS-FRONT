bb8d0d532d88ccdb22c4b152b84877a48ae6578e
Wfcmg <wfcarrionm@gmail.com>
2026-04-27 09:00:08 -0500

    Agrega panel admin y flujo de login por roles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+﻿import { Component, signal } from '@angular/core';
 import { BuscarVuelos } from './pages/buscar-vuelos/buscar-vuelos';
 import { Login } from './pages/login/login';
 import { AuthService } from './services/auth';
+import { AdminDashboard } from './admin/admin-dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [BuscarVuelos, Login],
+  imports: [BuscarVuelos, Login, AdminDashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,8 +14,13 @@ export class App {
   protected readonly title = signal('booking-vuelos-front');
 
   mostrarLogin = false;
+  vistaActual: 'home' | 'admin' = 'home';
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) {
+    if (this.authService.obtenerRol() === 'ADMIN') {
+      this.vistaActual = 'admin';
+    }
+  }
 
   abrirLogin() {
     this.mostrarLogin = true;
@@ -24,7 +30,23 @@ export class App {
     this.mostrarLogin = false;
   }
 
+  procesarLogin(rol: string) {
+    this.cerrarLogin();
+
+    if (rol === 'ADMIN') {
+      this.vistaActual = 'admin';
+    } else {
+      this.vistaActual = 'home';
+    }
+  }
+
   cerrarSesion() {
     this.authService.logout();
+    this.vistaActual = 'home';
+  }
+
+  salirAdmin() {
+    this.authService.logout();
+    this.vistaActual = 'home';
   }
 }
